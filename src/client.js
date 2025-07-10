c6a3e0d01ebef80188a100f834bb9b544f61e49b
statmano <69440675+statmano@users.noreply.github.com>
2025-07-09 20:59:27 -0400

    Update client.js

--- src/client.js
+++ src/client.js
@@ -12,7 +12,7 @@ $("#Records").click(function(){
 });
 
 $("#Rulebook").click(function(){
-  window.location = "https://njcorulebook.onrender.com/", "_blank");
+  window.location = ("https://njcorulebook.onrender.com/", "_blank");
 });
 
 $("#Recaps").click(function(){
